8623a2b5a65c3ab5c1f02b963d612fce2cf6036b
Miguel Miguel <miguel.miguel@bias.com.mx>
2018-04-03 10:44:47 -0500

    Modulo cfdi_nomina
    [FIX] -- Asiento.

--- cfdi_nomina/models/hr_payslip.py
+++ cfdi_nomina/models/hr_payslip.py
@@ -6,6 +6,7 @@ import odoo
 
 from odoo import api, fields, models, tools, _, registry
 from odoo.exceptions import UserError, RedirectWarning, ValidationError
+from odoo.tools import float_compare, float_is_zero
 from odoo.tools.safe_eval import safe_eval
 from odoo.api import Environment
 
@@ -463,6 +464,93 @@ class HrPayslip(models.Model):
 
 
     @api.multi
+    def action_payslip_done_nomina(self):
+        precision = self.env['decimal.precision'].precision_get('Payroll')
+        for slip in self:
+            line_ids = []
+            debit_sum = 0.0
+            credit_sum = 0.0
+            date = slip.date or slip.date_to
+
+            name = _('Payslip of %s') % (slip.employee_id.name)
+            move_dict = {
+                'narration': name,
+                'ref': slip.number,
+                'journal_id': slip.journal_id.id,
+                'date': date,
+            }
+            for line in slip.details_by_salary_rule_category:
+                amount = slip.credit_note and -line.total or line.total
+                if float_is_zero(amount, precision_digits=precision):
+                    continue
+                debit_account_id = line.salary_rule_id.account_debit.id
+                credit_account_id = line.salary_rule_id.account_credit.id
+
+                if debit_account_id:
+                    debit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=False),
+                        'account_id': debit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount > 0.0 and amount or 0.0,
+                        'credit': amount < 0.0 and -amount or 0.0,
+                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(debit_line)
+                    debit_sum += debit_line[2]['debit'] - debit_line[2]['credit']
+
+                if credit_account_id:
+                    credit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=True),
+                        'account_id': credit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount < 0.0 and -amount or 0.0,
+                        'credit': amount > 0.0 and amount or 0.0,
+                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(credit_line)
+                    credit_sum += credit_line[2]['credit'] - credit_line[2]['debit']
+
+            if float_compare(credit_sum, debit_sum, precision_digits=precision) == -1:
+                acc_id = slip.journal_id.default_credit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Credit Account!') % (slip.journal_id.name))
+                adjust_credit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': 0.0,
+                    'credit': debit_sum - credit_sum,
+                })
+                line_ids.append(adjust_credit)
+
+            elif float_compare(debit_sum, credit_sum, precision_digits=precision) == -1:
+                acc_id = slip.journal_id.default_debit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Debit Account!') % (slip.journal_id.name))
+                adjust_debit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': credit_sum - debit_sum,
+                    'credit': 0.0,
+                })
+                line_ids.append(adjust_debit)
+            move_dict['line_ids'] = line_ids
+            move = self.env['account.move'].create(move_dict)
+            slip.write({'move_id': move.id, 'date': date})
+            move.post()
+
+    @api.multi
     def action_payslip_done(self):
         res = False
         for rec in self:
@@ -484,12 +572,14 @@ class HrPayslip(models.Model):
                 self.action_raise_message("Error al Generar el XML \n\n %s "%( message.upper() ))
                 return False
             if rec.total < 0:
+                self.action_raise_message("Error al Generar el XML \n\n No se puede timbrar nominas con importe negativos")
                 return True
 
             self.action_write_date_invoice_cfdi(rec.date_invoice_cfdi, self.id)
             res = rec.action_create_cfdi()
             if res == True:
-                logging.info('Action Done 01 %s '%(rec.number) )
+                rec.action_payslip_done_nomina()
+                # self.action_raise_message("Nomina timbrada")
                 rec.write({'state': 'done'})
         return res
 
